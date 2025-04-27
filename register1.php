4d84bcdfb28381a1134410018cc060a487a7ebf1
vijay14193 <119676208+vijay14193@users.noreply.github.com>
2025-04-27 22:41:16 +0530

    Update register1.php

--- register1.php
+++ register1.php
@@ -1,30 +1,19 @@
 <?php
-// Connect to Database
-/*$servername = "localhost";
-$username = "your_db_username";
-$password = "your_db_password";
-$database = "your_db_name";*/
-
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "tourism"; // Your database name
-// Create connection
+$dbname = "tourism"; 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-// Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-// If form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = mysqli_real_escape_string($conn, $_POST['name']);
     $username = mysqli_real_escape_string($conn, $_POST['username']);
     $email = mysqli_real_escape_string($conn, $_POST['email']);
-    $password = password_hash($_POST['password'], PASSWORD_DEFAULT); // Encrypt password
-
-    // Insert into database
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT); 
     $sql = "INSERT INTO users (name, username, email, password) VALUES ('$name', '$username', '$email', '$password')";
 
     if (mysqli_query($conn, $sql)) {
@@ -33,7 +22,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "Error: " . mysqli_error($conn);
     }
 }
-
-// Close connection
 mysqli_close($conn);
 ?>
