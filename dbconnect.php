9e69a215765512d8660cec91b357efa59d2a5576
vijay14193 <119676208+vijay14193@users.noreply.github.com>
2025-04-27 22:35:48 +0530

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -2,11 +2,10 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "tourism"; // Your database name
+$dbname = "tourism"; 
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-// Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
